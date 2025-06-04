4650d44533ed5506baffe7ca1d0717269656837b
Rajathk6 <rajathkumar120@gmail.com>
2025-06-04 19:39:04 +0530

    program 2 solved

--- src/One.java
+++ src/One.java
@@ -1,7 +1,7 @@
 public class One {
     public static void main(String[] args) {
-        //  Problem 1: Positive or Negative Number
-        int num = -0;
+        // Problem 1: Positive or Negative Number
+        int num = -435;
         String ans = positiveNegative(num);
         System.out.println(ans);
     }
@@ -9,7 +9,7 @@ public class One {
     public static String positiveNegative(int num) {
         if (num > 0) {
             return "positive";
-        } else if(num < 0) {
+        } else if (num < 0) {
             return "negative";
         }
         return "zero";
---
+++ src/Two.java
@@ -0,0 +1,17 @@
+public class Two {
+    public static void main(String[] args) {
+        // Problem 2: Even or Odd Numbers
+        int num = 1000000;
+        String ans = evenOdd(num);
+        System.out.println(ans);
+    }
+
+    public static String evenOdd(int num) {
+        if (num == 0) {
+            return "Even";
+        } else if(num % 2 == 0) {
+            return "Even";
+        }
+        return "Odd";
+    }
+}
